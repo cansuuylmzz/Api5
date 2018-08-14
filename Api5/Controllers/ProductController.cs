41d55d5db0994ba0f3b27aded368560e21b6cd11
cansuuylmzz <38895480+cansuuylmzz@users.noreply.github.com>
2018-08-14 20:28:41 +0300

    Initial commit

---
+++ Api5/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Api5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api5.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult PostProduct(Product data)
+        {
+            if (true)
+            {
+                return Ok(data);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ Api5/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api5.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [Range(0,999)]
+        public double Weight { get; set; }
+    }
+}
